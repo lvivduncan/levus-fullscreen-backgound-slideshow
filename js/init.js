da2af4c3a5da59908307019018ac0aa21f4bc44a
lvivduncan <lvivduncan@gmail.com>
2019-09-23 17:23:44 +0300

    Webkit

--- js/init.js
+++ js/init.js
@@ -5,7 +5,9 @@ const images = document.querySelectorAll('.cb-slideshow span');
 // пробігаємо методом forEach і додаємо плавний ефект
 if(images !== null){
 	images.forEach( (item, i) => {
+		item.style.WebkitAnimationDelay = i * 6 + 's';
 		item.style.animationDelay = i * 6 + 's';
+		item.style.WebkitAnimationDuration = images.length * 6 + 's';
 		item.style.animationDuration = images.length * 6 + 's';
 	});
 }
@@ -16,7 +18,9 @@ const signatures = document.querySelectorAll('.cb-slideshow div');
 // пробігаємо методом forEach і додаємо плавний ефект
 if(signatures !== null){
 	signatures.forEach( (item, i) => {
+		item.style.WebkitAnimationDelay = i * 6 + 's';
 		item.style.animationDelay = i * 6 + 's';
+		item.style.WebkitAnimationDuration = images.length * 6 + 's';
 		item.style.animationDuration = images.length * 6 + 's';
 	});
 }
